06e51ddad01c52d7d9bf612588fd66df03c73948
Bratwurstinator <84632948+Bratwurstinator@users.noreply.github.com>
2022-10-17 21:15:36 +0200

    added support for other types of def, i guess

--- floors.cpp
+++ floors.cpp
@@ -34,32 +34,43 @@ void makeFloors(std::string path, std::vector<std::vector<std::string>> &materia
 	std::string line;
 	std::string parsed;
 	std::string filename;
+	std::string defType = "";
 	while (std::getline(file, line))
 	{
-		if(line.find("<TerrainDef") != std::string::npos){
-			parsed = "";
+		size_t pos, pos2, pos3;
+		if(defType == "" && (pos = line.find("<")) != std::string::npos){ //Identify uppermost node. Could use an XML parser but those are slooow.
+			pos2 = line.find(" ", pos);
+			pos3 = line.find(">", pos);
+			defType = line.substr(pos+1, (pos2 < pos3 ? pos2-pos-1 : pos3-pos-1));
+			std::cout << defType << " " << pos << " "<< pos2 << " " << pos3 << std::endl;
 		}
-		if(line.find("<defName>") != std::string::npos){
-			filename = line;
-			int pos, npos;
-			while((pos = filename.find("[")) != std::string::npos && (npos = filename.find("]", pos)) != std::string::npos ){filename.replace(pos, npos-pos+1, "");}
-			while((pos = filename.find("<")) != std::string::npos && (npos = filename.find(">", pos)) != std::string::npos ){filename.replace(pos, npos-pos+1, "");}
-			while((pos = filename.find(" ")) != std::string::npos){filename.replace(pos, 1, "");} //Extract filename. Possible via regex but nah
-			filename += "_MaterialDefs.xml";
-		}
-		parsed += line + "\n";
-		if(line.find("</TerrainDef>") != std::string::npos){
-			std::ofstream output(filename);
-			output << "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" << std::endl << "<Defs>" << std::endl;
-				for(int i = 1; i < materials.size(); i++){
-					int pos;
-					std::string parsed2 = parsed;
-					for(int j = 0; j < materials[i].size(); j++){ //create new def per material
-						while((pos = parsed2.find(materials[0][j])) != std::string::npos){parsed2.replace(pos, materials[0][j].length(), materials[i][j]);}
+		if(defType != ""){
+			if(line.find("<"+defType) != std::string::npos){
+				parsed = "";
+			}
+			if(line.find("<defName>") != std::string::npos){
+				filename = line;
+				int pos, npos;
+				while((pos = filename.find("[")) != std::string::npos && (npos = filename.find("]", pos)) != std::string::npos ){filename.replace(pos, npos-pos+1, "");}
+				while((pos = filename.find("<")) != std::string::npos && (npos = filename.find(">", pos)) != std::string::npos ){filename.replace(pos, npos-pos+1, "");}
+				while((pos = filename.find(" ")) != std::string::npos){filename.replace(pos, 1, "");} //Extract filename. Possible via regex but nah
+				filename += "_MaterialDefs.xml";
+			}
+			parsed += line + "\n";
+			if(line.find("</"+defType+">") != std::string::npos){
+				std::ofstream output(filename);
+				output << "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" << std::endl << "<Defs>" << std::endl;
+					for(int i = 1; i < materials.size(); i++){
+						int pos;
+						std::string parsed2 = parsed;
+						for(int j = 0; j < materials[i].size(); j++){ //create new def per material
+							while((pos = parsed2.find(materials[0][j])) != std::string::npos){parsed2.replace(pos, materials[0][j].length(), materials[i][j]);}
+						}
+						output << std::endl << parsed2;
 					}
-					output << std::endl << parsed2;
-				}
-			output << std::endl << "</Defs>";
+				output << std::endl << "</Defs>";
+				defType = "";
+			}
 		}
 	}
 	
